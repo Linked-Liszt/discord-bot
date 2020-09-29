7d540668c188df77545859a5c6c220ff4a35347c
unknown <18706708+Oxymoren@users.noreply.github.com>
2020-09-29 14:57:40 -0500

    Add working scrape script

--- scripts/data_utils.py
+++ scripts/data_utils.py
@@ -1,6 +1,6 @@
 TOKEN_LOCATION = "../configs/bot.txt"
 
-def get_token(token_fp=TOKEN_LOCATION: str) -> str:
+def get_token(token_fp:str = TOKEN_LOCATION) -> str:
     with open(token_fp, 'r') as token_f:
-        token = token_f.read()
+        token = token_f.read().strip()
     return token
--- scripts/scrape_raw_data.py
+++ scripts/scrape_raw_data.py
@@ -1,7 +1,9 @@
 import json
+import data_utils as du
+import os
 
 CONFIG_LOCATION = "../configs/scrape_config.json"
-EXPORTER_LOCATION = ""
+EXPORTER_LOCATION = "..\\export_cli\\DiscordChatExporter.Cli.exe"
 """
 JSON Structure:
 [
@@ -10,5 +12,12 @@ JSON Structure:
 ]
 """
 
-def script_main():
-    token =
+def export_guilds() -> None:
+    token = du.get_token()
+    with open(CONFIG_LOCATION, 'r') as config_f:
+        guilds = json.load(config_f)
+    for guild in guilds:
+        os.system(f"{EXPORTER_LOCATION} exportguild -t {token} -b -g {guild} -o ../data_raw -f csv")
+
+if __name__ == "__main__":
+    export_guilds()
