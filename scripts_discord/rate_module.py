0d81d4c78bf159a20fcd67f035f53648ada50548
unknown <18706708+Oxymoren@users.noreply.github.com>
2020-10-05 13:02:43 -0500

    Create rate module skeleton

--- scripts_discord/rate_module.py
+++ scripts_discord/rate_module.py
@@ -1,2 +1,12 @@
 import discord
-import discord_module
+import discord_module as dm
+
+class Rate(dm.DiscordModule):
+    def __init__(self):
+        self.info = ""
+        self.commands = {
+            "rate": ""
+        }
+
+    def handle_message(self, message: discord.Message):
+        pass
